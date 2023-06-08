af49c2ca33c5df33df41fe41c052ffb94ec55735
oSamDavis <osamdavis@gmail.com>
2023-06-08 00:27:03 -0400

    Initial Commit

---
+++ Sam-Sam-Collab/Sam_Sam_CollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sam_Sam_CollabApp.swift
+//  Sam-Sam-Collab
+//
+//  Created by Sam Davis Omekara  on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Sam_Sam_CollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
